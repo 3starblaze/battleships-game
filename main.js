e0068c5ae842b7b778593e763ce87359d873efae
Deniss Fjodorovs <3starblaze@gmail.com>
2020-02-28 07:24:06 +0200

    Add coordinate parameters and length property

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const Ship = function() {
+const Ship = function(x1, y1, x2, y2) {
+    const length = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
+
+    return { length };
 }
 
 module.exports = Ship;
--- ship.test.js
+++ ship.test.js
@@ -1,5 +1,9 @@
 const Ship = require('./main.js');
 
-test('Ship exists', () => {
-    expect(Ship()).toBe(undefined);
+test('Properly calculated length', () => {
+    expect(Ship(2, 3, 6, 3).length).toBe(4);
+});
+
+test('Calculated length with flipped coordinates', () => {
+    expect(Ship(6, 3, 2, 3).length).toBe(4);
 });
