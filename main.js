27382e01970902f9646e7a7762c756f65c9a29be
Deniss Fjodorovs <3starblaze@gmail.com>
2020-02-28 18:48:01 +0200

    Add hit and isSunk functions

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const Ship = function(length) {
     if (length <= 0) throw 'Length has to be positive!'
-    return { length };
+    let hitShipCells = Object.seal(new Array(length).fill(false));
+    let hitCells = 0;
+
+    const hit = function(i) {
+        if (i >= length || i < 0) throw "'i' is out of bounds!";
+        hitShipCells[i] = true;
+    }
+
+    const isSunk = function() {
+        return hitShipCells.reduce((a, b) => a + b) == length;
+    }
+
+    return { length, hit, isSunk };
 }
 
 module.exports = Ship;
--- ship.test.js
+++ ship.test.js
@@ -11,3 +11,30 @@ test('Can\'t pass negative length', () => {
 test('Can\'t pass 0 length', () => {
     expect(() => Ship(0)).toThrow()
 });
+
+test('Positive out-of-bound hit should throw', () => {
+    expect(() => Ship(3).hit(3)).toThrow();
+});
+
+test('Negative out-of-bound hit should throw', () => {
+    expect(() => Ship(4).hit(-3)).toThrow();
+});
+
+test('Ship can be sunk', () => {
+    let myShip = Ship(3);
+    myShip.hit(0);
+    myShip.hit(1);
+    myShip.hit(2);
+    expect(myShip.isSunk()).toBe(true);
+});
+
+test('Ship is not sunk initially', () => {
+    expect(Ship(2).isSunk()).toBe(false);
+});
+
+test('Ship can\'t be sunk by hitting 1 spot continuously', () => {
+    let myShip = Ship(2);
+    myShip.hit(0);
+    myShip.hit(0);
+    expect(myShip.isSunk()).toBe(false);
+});
